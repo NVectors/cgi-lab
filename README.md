affa3e01d422f2963ed0fe2bcc5227fc3bebeae3
Victor Nguyen <46537385+NVectors@users.noreply.github.com>
2023-01-25 19:43:08 -0700

    Q5-Q8

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ References:
 * [json.dumps](https://www.geeksforgeeks.org/json-dumps-in-python/)
 * [Content-Type](https://www.freecodecamp.org/news/what-is-the-correct-content-type-for-json-request-header-mime-type-explained/)
 * [Environment variables](https://www.pythonstudio.us/introduction-2/environment-variables.html)
+* [Cookies](https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies)
--- hello.py
+++ hello.py
@@ -5,7 +5,6 @@ import json
 
 print("Content-Type: text/plain")
 print("")
-print("<p> Hello World <p>")
 
 # Q1 Inspect all environment variables
 # os.environ returns a dictionary=> environment variables: value
@@ -19,11 +18,69 @@ json_object = json.dumps(dict(os.environ), indent=1)
 print(json_object)
 
 # Q2 Return values of the query parameter
-for param in os.environ.keys():
-    if param == "QUERY_STRING":
-        print("%s: %s" % (param, os.environ[param]))
+param = "QUERY_STRING"
+print("%s: %s" % (param, os.environ[param]))
 
 # Q3 Return value of the user's browser
-for param in os.environ.keys():
-    if param == "HTTP_USER_AGENT":
-        print("%s: %s" % (param, os.environ[param]))
+param = "HTTP_USER_AGENT"
+print("%s: %s" % (param, os.environ[param]))
+
+####################################################################################################
+from templates import login_page, secret_page, after_login_incorrect
+import secret
+import cgi, cgitb
+from http.cookies import SimpleCookie
+
+# Python 3.7 versus Python 3.8
+try:
+    from cgi import escape #v3.7
+except:
+    from html import escape #v3.8
+
+cgitb.enable()
+
+# Q4 POSTed data to the CGI script
+#print(login_page())
+form = cgi.FieldStorage()
+
+# Get the data from the form fields
+usernameEntered = form.getvalue('username')
+passwordEntered = form.getvalue('password')
+
+form_ok = (usernameEntered == secret.username and passwordEntered == secret.password)
+
+#Q5 Set a cookie if login is correct
+param = "HTTP_COOKIE"
+cookie = SimpleCookie(os.environ[param])
+# By default
+cookie_username = None
+cookie_password = None
+# Set cookie
+if cookie.get("username"):
+   cookie_username = cookie.get("username").value
+if cookie.get("password"):
+   cookie_password = cookie.get("password").value
+
+cookie_ok = (cookie_username == secret.username and cookie_password == secret.password)
+
+# Check for cookie
+if (cookie_ok):
+    usernameEntered = cookie_username
+    passwordEntered = cookie_password
+
+print("Content-Type: text/html")
+
+# Check login info
+if (form_ok):
+    # Set the cookie
+    print("Set-Cookie: username=", usernameEntered)
+    print("Set-Cookie: password=", passwordEntered)
+print("")
+
+if (not usernameEntered and not passwordEntered):
+    print(login_page())
+elif (form_ok):
+    print(secret_page(usernameEntered, passwordEntered))
+else:
+    print(after_login_incorrect())
+
